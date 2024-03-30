a6aa2381896447ef28f6c8f3627f1e3d68ae0688
arrancarr <53247392+arrancarr@users.noreply.github.com>
2024-03-30 14:23:05 +0800

    set up action

---
+++ dumpcsv.py
@@ -0,0 +1,69 @@
+import os
+import pytz
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import datetime
+
+legend = [
+    {"Code": "BPlus", "Name": "Bugis +"},
+    {"Code": "CQ", "Name": "Clarke Quay"},
+    {"Code": "FN", "Name": "Funan"},
+    {"Code": "PS", "Name": "Plaza Singapura"},
+    {"Code": "RCS", "Name": "Raffles City"},
+    {"Code": "TAO", "Name": "The Atrium@Orchard"},
+    {"Code": "BM", "Name": "Bedok Mall"},
+    {"Code": "TM", "Name": "Tampines Mall"},
+    {"Code": "BPP", "Name": "Bukit Panjang Plaza"},
+    {"Code": "J8", "Name": "Junction 8"},
+    {"Code": "LO", "Name": "Lot One"},
+    {"Code": "IMM", "Name": "IMM"},
+    {"Code": "WG", "Name": "Westgate"},
+    {"Code": "CG", "Name": "CapitaGreen"},
+    {"Code": "CT", "Name": "Capital Tower"},
+    {"Code": "SBR", "Name": "Six Battery Road"},
+    {"Code": "CS", "Name": "CapitaSpring"},
+]
+
+locations = []
+
+url = "https://justpark.capitaland.com/LotsAvail"
+
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"}
+r = requests.get(url, headers=headers)
+
+soup = BeautifulSoup(r.content, "html.parser")
+
+section = soup.find("div", attrs={"class": "lots-form"})
+boxes = section.find_all("div", attrs={"class": "listing-item"})
+
+for box in boxes:
+    div = box.find("div", attrs={"class": "lotscount"})
+    name = div['id'].split("onClickSeason")[1]
+    available_lots = div.find("span").text
+    
+    record = {
+        "Code": name,
+        "Available Lots": available_lots
+    }
+
+    locations.append(record)
+
+df_legend = pd.DataFrame(legend)
+df = pd.DataFrame(locations)
+df_final = df_legend.merge(df, left_on="Code", right_on="Code")
+
+# Timezone conversion to Singapore Time
+singapore_zone = pytz.timezone('Asia/Singapore')
+sg_time = datetime.now(singapore_zone).strftime("%Y-%m-%d_%H")
+
+# Create the tiprank folder if it doesn't exist
+if not os.path.exists('JustPark'):
+    os.makedirs('JustPark')
+
+# Create CSV filename with current date and time
+csv_filename = f"JustPark/JustPark_{sg_time}.csv"
+  
+# Dump dataframe to CSV
+df_final.to_csv(csv_filename, index=False)
+print(f"DataFrame dumped to {csv_filename}")
